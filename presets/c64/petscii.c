7e8525f5798595dced544e1387d3ab3b1bdfd6e0
Chris Garrett <chris@chrisg.com>
2025-10-06 11:56:54 +0100

    Deploy: update public release artifacts

--- presets/c64/petscii.c
+++ presets/c64/petscii.c
@@ -1,5 +1,5 @@
 /* OUTPUTS THE PETSCII GRAPHIC SYMBOLS */
-/* Equiv to BASIC CHR$(c)              */ 
+/* Equiv to BASIC CHR$(c) + POKEs      */ 
 
 #include <conio.h>
 #include <stdio.h>
@@ -12,24 +12,32 @@
 #include "common.h"
 //#link "common.c"
 
-void main(void) {
+void petscii() {
   
   int c,x,y;
+  
+  // Foreground colours
   char col1 = 5;
   char col2 = 154;  
-  clrscr();
-  c=64;
-  x=0;
-  y=0;
-  
 
-
-  // Colours
+  // Initial Colours
   bordercolor(COLOR_BLUE);
   textcolor(COLOR_WHITE);
 
   // set character set to uppercase + graphics characters
   printf("%c",142);
+
+ 
+  // ==================
+  // CHR$ 64-255
+  // ==================
+
+  
+  clrscr();
+  c=64;
+  x=0;
+  y=0;
+  
   
   while(x<40) {
     if(c==128) c=160;
@@ -61,5 +69,91 @@ void main(void) {
   }
   
   c=cgetc();
+
+  // ==================
+  // POKEs 0-127
+  // ==================
+  c=0;
+  x=0;
+  y=0;
+  clrscr();
   
+    while(x<40) {
+
+   
+    POKE(1024+(y*40+x),c);
+    gotoxy(x+2,y);
+    printf("%c%d",col1,c);
+    
+    
+    if(c<127)
+    {  
+      c++;
+    }
+    else
+    {
+
+      break;
+    }
+  
+    if(y<23)
+    {  
+      y++;
+    }
+    else
+    {
+
+      y=0;
+      if(c<96) x+=5; else x+=6;
+    }
+  }
+  
+  c=cgetc();
+  
+  // ==================
+  // POKEs 128-255
+  // ==================
+  c=128;
+  x=0;
+  y=0;
+  clrscr();
+  
+    while(x<40) {
+
+   
+    POKE(1024+(y*40+x),c);
+    gotoxy(x+2,y);
+    printf("%c%d",col2,c);
+    
+    
+    if(c<255)
+    {  
+      c++;
+    }
+    else
+    {
+
+      break;
+    }
+  
+    if(y<23)
+    {  
+      y++;
+    }
+    else
+    {
+
+      y=0;
+      x+=6;
+    }
+  }
+  
+  c=cgetc();
+  
+}
+
+void main(void) {
+  
+ while(1) petscii();
+ return;
 }
