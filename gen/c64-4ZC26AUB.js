922db161a690d20eae4e2bc0852ceff4b4fda07e
Chris Garrett <chris@chrisg.com>
2025-10-11 12:06:14 +0100

    Deploy: update public release artifacts

---
+++ gen/c64-4ZC26AUB.js
@@ -0,0 +1,2 @@
+import{a}from"./chunk-2GJEG4FN.js";import"./chunk-KXQZZRQB.js";import"./chunk-WAARL7ET.js";export{a as default};
+//# sourceMappingURL=c64-4ZC26AUB.js.map
--- gen/platform/c64.js
+++ gen/platform/c64.js
@@ -18,7 +18,6 @@ const C64_PRESETS = [
     { id: 'trek64.bas', name: 'Star Trek (BASIC)' },
     { id: 'controlcodes.bas', name: 'Control Codes Demo (BASIC)' },
     { id: 'hello.acme', name: 'Hello World (ACME)', category: 'Assembly (ACME)' },
-    { id: 'hello.s', name: 'Hello World (cc65)', category: 'Assembly (cc65)' },
     { id: 'screen_ram.c', name: 'Screen RAM', category: '8-bit Workshop Demos' },
     { id: 'siegegame.c', name: 'Siege Game' },
     { id: 'joymove.c', name: 'Sprite Movement' },
@@ -390,8 +389,6 @@ class C64ChipsPlatform {
         if (filename.toLowerCase().endsWith(".bas"))
             return "c64basic";
         if (filename.endsWith(".c"))
-            return "cc65";
-        if (filename.endsWith(".s"))
             return "cc65";
         if (filename.endsWith(".dasm"))
             return "dasm";
